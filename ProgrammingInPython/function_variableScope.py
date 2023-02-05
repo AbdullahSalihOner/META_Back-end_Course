7e0682eb0fbaeb3c8755ff92091efbdde7f092b1
AbdullahSalihOner <salihriyad@hotmail.com>
2023-02-05 20:57:26 +0300

    5subat23

---
+++ ProgrammingInPython/function_variableScope.py
@@ -0,0 +1,55 @@
+#Local Scope
+
+def get_total(a, b):
+    #local variable declared inside a functional function
+    total = a + b;
+    return total
+
+print(get_total(5,2))
+
+#Accessing variable outside of the function:
+#print(total)
+#NameError: name 'total' is not defined
+
+
+#Enclosing Scope
+
+def get_total(a, b):
+    #enclosed variable declared inside a function
+    total = a + b
+
+    def double_it():
+        #local variable
+        double = total * 2
+        print(double)
+
+    double_it()
+    #double variable will not be accessible
+    print(double)
+
+    return total
+
+
+#global Scope
+
+
+special = 5
+
+def get_total(a, b):
+    #enclosed scope variable declared inside a function
+    total = a + b
+    print(special)
+
+    def double_it():
+        #local variable
+        double = total * 2
+        print(special)
+
+    double_it()
+
+    return total
+
+
+#built - in scope 
+#Built-in scope refers to the reserved keywords that Python uses for its built-in functions,
+#such as print, def, for, in, and so forth.  Functions with built-in scope can be accessed at any level.
