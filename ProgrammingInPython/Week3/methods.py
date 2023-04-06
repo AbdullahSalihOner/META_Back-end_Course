b7f14374e6a2763f9db997174bb7041d94ba0648
AbdullahSalihOner <salihriyad@hotmail.com>
2023-04-06 14:40:15 +0300

    06/04

--- ProgrammingInPython/Week3/methods.py
+++ ProgrammingInPython/Week3/methods.py
@@ -28,3 +28,27 @@ class F(E,D,C):
 f = F()
 print(f.d())
 print(F.mro())
+
+# How can I write factorial method
+ #How can I use factorial
+
+
+
+# Path: Week3\methods.py
+class A:
+    def d(self):
+        return "Function inside A"
+    
+    def factorial(self, n):
+        if n == 0:
+            return 1
+        else:
+            return n * self.factorial(n-1)
+    
+   
+
+a = A()
+print(a.factorial(5))
+
+
+
